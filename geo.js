a7b56f9c9e49e80982ea56b329e8776df529fd66
Mehedihasantushar1993 <mdmehedidhl@gmail.com>
2023-08-18 19:06:10 +0600

    js work

--- geo.js
+++ geo.js
@@ -21,28 +21,47 @@ function calculaterectangleeArea() {
   const widthValueText = widthField.value;
   const width = parseFloat(widthValueText);
   console.log(width);
+   // validate width input
+
+  
   // get rectangle length value
   const lengthField = document.getElementById('rectangle-length');
   const lengthValueText = lengthField.value;
   const length = parseFloat(lengthValueText);
   console.log(length);
-  const area = width * length;
+  // validate length input
+if(isNaN(width) || isNaN(length)){
+  alert('Please insert a number');
+  return;
+}
+  
+  area = width * length;
   console.log(area);
   // show rectangle area
   const rectangleareaSpan = document.getElementById('rectangle-area');
   rectangleareaSpan.innerText = area;
+
+ 
+
 }
+
 
 
 // Reusable function --- reduce duplicate code
 function calculateParallelogrameArea() {
   const base = getInputValue('parallelogram-base');
-  console.log(base);
+ 
   const height = getInputValue('parallelogram-height');
-  console.log(height);
+  // validation input
+  if(isNaN(base) || isNaN(height)){
+  alert('Please insert a number');
+  return;
+  }
+  
   const area = base * height;
   console.log(area);
   setElementInnerText('parallelogram-area', area);
+  addToCalculationEntry('Parallelogram:', area);
 }
 function calculateEllipseeArea() {
   const majoRadious = getInputValue('ellipse-major-radious');
@@ -50,6 +69,7 @@ function calculateEllipseeArea() {
   const area = 3.1416 * majoRadious * minorRadious;
   const areaTwoDecimal = area.toFixed(2);
   setElementInnerText('ellipse-area', areaTwoDecimal);
+  addToCalculationEntry("Ellipse", areaTwoDecimal);
 }
 function getInputValue(fieldId) {
   const inputField = document.getElementById(fieldId);
@@ -64,4 +84,25 @@ function setElementInnerText(elementId, area) {
    const element = document.getElementById(elementId);
   element.innerText = area;
 
+}
+
+
+// add to calculation entry
+/**
+ * get the elemnet where you add the dynamic HTML
+ * create a element you want to add
+ *  if needed added some class
+ * set inner HTML.it could be dynamic Template string
+ * appened the created element as a child of the parent
+ * */ 
+
+
+function addToCalculationEntry(areaType, area) {
+  console.log(areaType + ' ' + area);
+  const caculationEntry = document.getElementById('calculation-id');
+  const count = caculationEntry.childElementCount;
+  const p = document.createElement('p');
+  p.classList.add('my-4');
+  p.innerHTML = ` ${count+1}  ${areaType} ${area} cm <sup>2</sup> <button class="btn btn-primary btn-sm">Convert</button>`;
+  caculationEntry.appendChild(p);
 }
